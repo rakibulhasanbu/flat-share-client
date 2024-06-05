235ad36be17220ff735f9e61984747015f61890c
rakibulhasanbu <rakibulhasanbu@gmail.com>
2024-06-05 19:44:25 +0600

    updated and fixes all data

--- app/flat-details/[id]/page.tsx
+++ app/flat-details/[id]/page.tsx
@@ -8,59 +8,6 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { ImSpinner2 } from 'react-icons/im';
 import { toast } from 'react-toastify';
 
-const product = {
-    name: 'Basic Tee 6-Pack',
-    price: '$192',
-    href: '#',
-    breadcrumbs: [
-        { id: 1, name: 'Men', href: '#' },
-        { id: 2, name: 'Clothing', href: '#' },
-    ],
-    images: [
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-            alt: 'Two each of gray, white, and black shirts laying flat.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
-            alt: 'Model wearing plain black basic tee.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
-            alt: 'Model wearing plain gray basic tee.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
-            alt: 'Model wearing plain white basic tee.',
-        },
-    ],
-    colors: [
-        { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
-        { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
-        { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
-    ],
-    sizes: [
-        { name: 'XXS', inStock: false },
-        { name: 'XS', inStock: true },
-        { name: 'S', inStock: true },
-        { name: 'M', inStock: true },
-        { name: 'L', inStock: true },
-        { name: 'XL', inStock: true },
-        { name: '2XL', inStock: true },
-        { name: '3XL', inStock: true },
-    ],
-    description:
-        'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-    highlights: [
-        'Hand cut and sewn locally',
-        'Dyed with our proprietary colors',
-        'Pre-washed & pre-shrunk',
-        'Ultra-soft 100% cotton',
-    ],
-    details:
-        'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-}
-
 type TInputs = {
     message: string;
 };
@@ -77,7 +24,7 @@ export default function FlatDetails() {
 
     const { data, isLoading, isError } = useGetFlatByIdQuery(id);
     const [bookingFlat, { isLoading: flatBookingLoading }] = useBookingFlatMutation()
-
+    console.log(data);
     const onSubmit: SubmitHandler<TInputs> = async (data) => {
         const submitData = {
             ...data, flatId: id
@@ -123,7 +70,7 @@ export default function FlatDetails() {
 
                     {/* Options */}
                     <div className="mt-4 lg:row-span-3 lg:mt-0">
-                        <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
+                        <p className="text-3xl tracking-tight text-gray-900">${data?.data?.amount}</p>
 
                         <form className="mt-10" onSubmit={handleSubmit(onSubmit)}>
                             <AppFormInput
@@ -148,30 +95,32 @@ export default function FlatDetails() {
                             <h3 className="sr-only">Description</h3>
 
                             <div className="space-y-6">
-                                <p className="text-base text-gray-900">{product.description}</p>
+                                <p className="text-base text-gray-900">{data?.data.description}</p>
                             </div>
                         </div>
 
                         <div className="mt-10">
-                            <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+                            <h3 className="text-sm font-medium text-gray-900">Amenities</h3>
 
                             <div className="mt-4">
                                 <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                                    {product.highlights.map((highlight) => (
-                                        <li key={highlight} className="text-gray-400">
-                                            <span className="text-gray-600">{highlight}</span>
+                                    {data?.data?.amenities.map((amn: string) => (
+                                        <li key={amn} className="text-gray-400">
+                                            <span className="text-gray-600">{amn}</span>
                                         </li>
                                     ))}
                                 </ul>
                             </div>
                         </div>
 
-                        <div className="mt-10">
-                            <h2 className="text-sm font-medium text-gray-900">Details</h2>
+                        <div className="mt-10 grid grid-cols-2 gap-4">
+                            <h2 className="text-sm font-medium text-gray-900"><span className='text-base'>Location: </span> {data?.data?.location}</h2>
+                            <h2 className="text-sm font-medium text-gray-900"><span className='text-base'>Square Feet: </span> {data?.data?.squareFeet}</h2>
+                            <h2 className="text-sm font-medium text-gray-900"><span className='text-base'>Total Bedrooms: </span> {data?.data?.totalBedrooms}</h2>
+                            <h2 className="text-sm font-medium text-gray-900"><span className='text-base'>Total Rooms: </span> {data?.data?.totalRooms}</h2>
+                            <h2 className="text-sm font-medium text-gray-900"><span className='text-base'>Advance Amount: </span> {data?.data?.advanceAmount}</h2>
 
-                            <div className="mt-4 space-y-6">
-                                <p className="text-sm text-gray-600">{product.details}</p>
-                            </div>
+
                         </div>
                     </div>
                 </div>
